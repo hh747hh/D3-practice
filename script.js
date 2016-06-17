37a9a9937bcb59280b7b03d7b858d78b884829d6
Woo Yun <hh747hh@gmail.com>
2016-06-17 13:15:48 -0400

    Using transitions and animations

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var bardata = [20,30,130,15,45,56,150,20,30,130,15,45,56,150];
+var bardata = [];
+
+for (var i=0; i<50; i++){
+  bardata.push(Math.random())
+}
 
 var height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
+
+var tempColor;
+
+var colors = d3.scale.linear()
+.domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
+.range(['#B58929','#C61C6F', '#268BD2', '#85992C'])
 
 var yScale = d3.scale.linear()
         .domain([0, d3.max(bardata)])
@@ -13,20 +23,46 @@ var xScale = d3.scale.ordinal()
         .domain(d3.range(0, bardata.length))
         .rangeBands([0, width])
 
-d3.select('#chart').append('svg')
+var myChart = d3.select('#chart').append('svg')
     .attr('width', width)
     .attr('height', height)
-    .style('background', '#C9D7D6')
     .selectAll('rect').data(bardata)
     .enter().append('rect')
-        .style('fill', '#C61C6F')
-        .attr('width', xScale.rangeBand())
-        .attr('height', function(d){
-            return yScale(d);
+        .style('fill', function(d,i){
+          return colors(i);
         })
+        .attr('width', xScale.rangeBand())
+
     .attr('x', function(d,i){
       return xScale(i);
     })
-    .attr('y', function(d){
-      return height - yScale(d);
-    })
+    .attr('height', 0)
+    .attr('y', height)
+
+  .on('mouseover', function(d){
+    tempColor = this.style.fill;
+    d3.select(this)
+        .transition()
+        .style('opacity', .5)
+        .style('fill', 'yellow')
+  })
+
+  .on('mouseout', function(d) {
+    d3.select(this)
+        .transition()
+        .style('opacity', 1)
+        .style('fill', tempColor)
+  })
+
+myChart.transition()
+.attr('height', function(d){
+  return yScale(d);
+  })
+.attr('y', function(d){
+  return height - yScale(d);
+})
+.delay(function(d,i) {
+  return i * 20;
+})
+.duration(1000)
+.ease('elastic')
